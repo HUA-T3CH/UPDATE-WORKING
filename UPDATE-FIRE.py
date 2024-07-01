b5fe51a6c4fb1cabadf9eb1eef9f2c3745b75829
HUA-T3CH <mryeasintechnology@gmail.com>
2024-07-01 19:49:36 +0600

    UPDATE-FIRE.py

---
+++ UPDATE-FIRE.py
@@ -0,0 +1,49 @@
+#coding=utf-8
+
+import os, sys, platform
+
+os.system('xdg-open https://facebook.com/groups/658593889736993/')
+
+try:
+
+    if sys.argv[1]=='update':
+
+        os.system('rm -rf UMW64.cpython-311.so UMW.cpython-311.so')
+
+except:
+
+    pass
+
+os.system('rm -rf UMW64.cpython-311.so UMW.cpython-311.so')
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    if not os.path.isfile('UMW64.cpython-311.so'):
+
+        os.system('curl https://raw.githubusercontent.com/HUA-T3CH/UPDATE-WORKING/blob/main/UMW64.cpython-311.so > UMW64.cpython-311.so') 
+
+        os.system("chmod 777 UMW64*")
+        
+        import UMW64
+
+    else:
+
+        import UMW64
+
+elif bit == '32bit':
+
+    if not os.path.isfile('UMW.cpython-311.so'):
+
+        os.system('curl https://raw.githubusercontent.com/HUA-T3CH/UPDATE-WORKING/blob/main/UMW.cpython-311.so > UMW.cpython-311.so')
+
+        os.system("chmod 777 UMW*")
+
+        import UMW
+
+    else:
+
+        import UMW
